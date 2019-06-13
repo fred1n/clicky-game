97ec4c331f057b275917e5f33e5f729b5de5cf01
fred1n <fred1n@hotmail.com>
2019-06-13 13:41:41 -0500

    intial file

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import "./App.css";
+
+function App() {
+  return (
+    <Wrapper>
+      <h1 className="title">Friends List</h1>
+      <FriendCard
+        name={friends[0].name}
+        image={friends[0].image}
+        occupation={friends[0].occupation}
+        location={friends[0].location}
+      />
+      <FriendCard
+        name={friends[1].name}
+        image={friends[1].image}
+        occupation={friends[1].occupation}
+        location={friends[1].location}
+      />
+      <FriendCard
+        name={friends[2].name}
+        image={friends[2].image}
+        occupation={friends[2].occupation}
+        location={friends[2].location}
+      />
+      <FriendCard
+        name={friends[3].name}
+        image={friends[3].image}
+        occupation={friends[3].occupation}
+        location={friends[3].location}
+      />
+    </Wrapper>
+  );
+}
+
+export default App;
