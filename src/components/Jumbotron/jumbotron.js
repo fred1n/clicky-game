1a2b8aad1047d8e6a541c014f907006889fafe42
fred1n <fred1n@hotmail.com>
2019-07-23 11:27:36 -0500

    setup for heroku

--- src/components/Jumbotron/jumbotron.js
+++ src/components/Jumbotron/jumbotron.js
@@ -5,7 +5,8 @@ import "./jumbotron.css";
 const Jumbotron = () => (
 	<header className = "header">
 		<h1> /n/n</h1>
-		<h2> Click an image to earn a point but you can't click on the same image more than once.</h2>
+		<h2> Click an image to earn a point but you can't click on the same image more than once.        </h2>
+		
 	</header>
 );
 
